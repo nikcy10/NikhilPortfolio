7a82c6c5b825807f21efdaff1f72cec58e1aa27d
Nikhil More <nikcy10.07@gmail.com>
2025-06-23 22:48:07 +0530

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -35,7 +35,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/More Nikhil Resume.pdf"
+                href="/Nikhil More Resume 1.pdf"
                 download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
